db3f04468ac99e5f82273f0c9c1a18cfb59f0b81
withinsea <withinsea@b33ab282-f52a-11de-9e29-7d1cd92887ef>
2010-01-07 14:41:08 +0000

    poms

--- trunk/izayoi-commons/src/main/java/org/withinsea/izayoi/commons/servlet/ByteArrayServletOutputStream.java
+++ trunk/izayoi-commons/src/main/java/org/withinsea/izayoi/commons/servlet/ByteArrayServletOutputStream.java
@@ -23,61 +23,55 @@
  */
 package org.withinsea.izayoi.commons.servlet;
 
-import java.io.IOException;
-
 import javax.servlet.ServletOutputStream;
+import java.io.IOException;
 
 public class ByteArrayServletOutputStream extends ServletOutputStream {
 
-	protected byte buf[];
+    protected byte buf[];
 
-	protected int count;
+    protected int count;
 
-	public ByteArrayServletOutputStream() {
-		this(32);
-	}
+    public ByteArrayServletOutputStream() {
+        this(32);
+    }
 
-	public ByteArrayServletOutputStream(int size) {
-		if (size < 0) {
-			throw new IllegalArgumentException("Negative initial size: " + size);
-		}
-		buf = new byte[size];
-	}
+    public ByteArrayServletOutputStream(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Negative initial size: " + size);
+        }
+        buf = new byte[size];
+    }
 
-	public synchronized byte toByteArray()[] {
-		return copyOf(buf, count);
-	}
+    public synchronized byte toByteArray()[] {
+        return copyOf(buf, count);
+    }
 
-	public synchronized void reset() {
-		count = 0;
-	}
+    public synchronized void reset() {
+        count = 0;
+    }
 
-	public synchronized int size() {
-		return count;
-	}
+    public synchronized int size() {
+        return count;
+    }
 
-	public void enlarge(int size) {
-		if (size > buf.length) {
-			buf = copyOf(buf, Math.max(buf.length << 1, size));
-		}
-	}
+    public void enlarge(int size) {
+        if (size > buf.length) {
+            buf = copyOf(buf, Math.max(buf.length << 1, size));
+        }
+    }
 
-	@Override
-	public synchronized void write(int b) throws IOException {
-		int newcount = count + 1;
-		enlarge(newcount);
-		buf[count] = (byte) b;
-		count = newcount;
-	}
+    @Override
+    public synchronized void write(int b) throws IOException {
+        int newcount = count + 1;
+        enlarge(newcount);
+        buf[count] = (byte) b;
+        count = newcount;
+    }
 
-	/**
-	 * copy from: jdk1.6, java.util.Arrays.copyOf(byte[] original, int
-	 * newLength)
-	 */
-	private static byte[] copyOf(byte[] original, int newLength) {
-		byte[] copy = new byte[newLength];
-		System.arraycopy(original, 0, copy, 0, Math.min(original.length,
-				newLength));
-		return copy;
-	}
+    private static byte[] copyOf(byte[] original, int newLength) {
+        byte[] copy = new byte[newLength];
+        System.arraycopy(original, 0, copy, 0, Math.min(original.length, newLength));
+        return copy;
+    }
 }
