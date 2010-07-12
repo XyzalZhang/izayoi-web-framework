0cdab5de1022a9a239fd8a0e83099f324280e50c
withinsea <withinsea@b33ab282-f52a-11de-9e29-7d1cd92887ef>
2010-07-12 06:48:03 +0000

    [fix]   1. fix jsp-as-script encoding

--- izayoi-core/src/main/java/org/withinsea/izayoi/core/interpret/JSP.java
+++ izayoi-core/src/main/java/org/withinsea/izayoi/core/interpret/JSP.java
@@ -99,7 +99,8 @@ public class JSP implements CompilableInterpreter {
     }
 
     protected static final String INTERPRET_PREFIX =
-            "<% if (" + JSP.class.getCanonicalName() + ".interpret(this, request)) return; %>";
+            "<%@ page contentType=\"text/html; charset=UTF-8\" pageEncoding=\"UTF-8\" %>" +
+                    "<% if (" + JSP.class.getCanonicalName() + ".interpret(this, request)) return; %>";
 
     protected class CompiledJSP implements CompiledInterpreter {
 
