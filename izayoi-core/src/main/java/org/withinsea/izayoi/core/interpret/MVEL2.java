7a603405a0fe64cc4a056cc5e703793ac6dc6fdf
withinsea <withinsea@b33ab282-f52a-11de-9e29-7d1cd92887ef>
2010-09-19 08:39:51 +0000

    [refactor]   1. made MVEL2 treat null variables as null, but not undefined.

--- izayoi-core/src/main/java/org/withinsea/izayoi/core/interpret/MVEL2.java
+++ izayoi-core/src/main/java/org/withinsea/izayoi/core/interpret/MVEL2.java
@@ -25,10 +25,13 @@
 package org.withinsea.izayoi.core.interpret;
 
 import org.mvel2.MVEL;
+import org.mvel2.integration.VariableResolver;
+import org.mvel2.integration.impl.MapVariableResolverFactory;
 import org.withinsea.izayoi.core.code.Code;
 import org.withinsea.izayoi.core.exception.IzayoiException;
 
 import javax.script.Bindings;
+import java.util.Map;
 
 /**
  * Created by Mo Chen <withinsea@gmail.com>
@@ -44,6 +47,65 @@ public class MVEL2 implements Interpreter {
         for (String claz : importedClasses) {
             importsEl += "import " + claz + ";";
         }
-        return (T) MVEL.eval(importsEl + code.getCode(), bindings);
+        return (T) MVEL.eval(importsEl + code.getCode(), new NullableVarMapVariableResolverFactory(bindings));
+    }
+
+    protected static class NullableVarMapVariableResolverFactory extends MapVariableResolverFactory {
+
+        public NullableVarMapVariableResolverFactory(Map variables) {
+            super(variables);
+        }
+
+        @Override
+        public VariableResolver getVariableResolver(String name) {
+            return super.isResolveable(name) ? super.getVariableResolver(name) : new NullVariableResolver(name);
+        }
+
+        @Override
+        public boolean isTarget(String name) {
+            return true;
+        }
+
+        @Override
+        public boolean isResolveable(String name) {
+            return true;
+        }
+    }
+
+    protected static class NullVariableResolver implements VariableResolver {
+
+        protected String name;
+
+        public NullVariableResolver(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public Class getType() {
+            return Object.class;
+        }
+
+        @Override
+        public void setStaticType(Class type) {
+        }
+
+        @Override
+        public int getFlags() {
+            return 0;
+        }
+
+        @Override
+        public Object getValue() {
+            return null;
+        }
+
+        @Override
+        public void setValue(Object value) {
+        }
     }
 }
